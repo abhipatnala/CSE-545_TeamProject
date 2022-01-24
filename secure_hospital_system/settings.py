8db759e035893b051f866498ec4ced169fd98ecb
Avinash Patnala <avinashpatnala@avinashs-mbp.3500.dhcp.asu.edu>
2022-01-23 18:23:50 -0700

    remove static file settings

--- secure_hospital_system/settings.py
+++ secure_hospital_system/settings.py
@@ -124,15 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = [
+    # os.path.join(PROJECT_ROOT, 'static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
