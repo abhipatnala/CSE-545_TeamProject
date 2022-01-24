3007238b2d8e19eea96b9a49fdedb422c1821998
Avinash Patnala <avinashpatnala@avinashs-mbp.3500.dhcp.asu.edu>
2022-01-23 17:57:36 -0700

    Adding django on heroku

--- secure_hospital_system/settings.py
+++ secure_hospital_system/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import django_on_heroku
+
+django_on_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
