03e34e1f4c910099e5b059f801d7e056e6515ef0
Avinash Patnala <avinashpatnala@avinashs-mbp.3500.dhcp.asu.edu>
2022-03-31 15:55:47 -0700

    fix bus in settings

--- secure_hospital_system/settings.py
+++ secure_hospital_system/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_mfa',
-    'axes'
+    'axes',
     'secure_hospital_system',
     'django_tables2',
     'django_filters',
