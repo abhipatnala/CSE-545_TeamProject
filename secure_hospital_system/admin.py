430830af0f9530078453fcc793e099b73556f363
Sabhariesh Hariharasudan <sabbhy1996@gmail.com>
2022-04-05 11:02:31 -0600

    Admin Remove models

--- secure_hospital_system/admin.py
+++ secure_hospital_system/admin.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 from .models import *
+import django_mfa
 #from django.contrib.auth.models import User
-admin.site.register(Roles)
+
 admin.site.register(SHSUser)
 admin.site.register(InsuranceProvider)
 admin.site.register(Patient)
-admin.site.register(Shift_Timings)
 admin.site.register(Doctor)
 admin.site.register(Doctor_availability_booked)
 admin.site.register(Records)
 admin.site.register(Payments)
 admin.site.register(Claim_Request)
-admin.site.register(Menu)
-admin.site.register(Menu_Mapping)
 admin.site.register(Lab_Test)
+admin.site.unregister(django_mfa.models.U2FKey)
