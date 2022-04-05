d24dcd671d6b54575f0a0e20419101225af2816f
Avinash Patnala <avinashpatnala@avinashs-mbp.3500.dhcp.asu.edu>
2022-04-04 20:49:06 -0700

    debug  to false

--- secure_hospital_system/settings.py
+++ secure_hospital_system/settings.py
@@ -32,7 +32,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # DEBUG = False
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'secure-hospital-v1.herokuapp.com']
